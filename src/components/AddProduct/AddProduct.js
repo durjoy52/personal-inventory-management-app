1cc03b0a62b5614a3fdf82c306a527cae3cdba13
durjoy sarkar <durjoywebdeveloper777@gmail.com>
2022-10-23 00:50:53 +0600

    addProduct route set

--- src/components/AddProduct/AddProduct.js
+++ src/components/AddProduct/AddProduct.js
@@ -1,21 +1,32 @@
 import React from "react";
 
 const AddProduct = () => {
+  const addProduct = (event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const price = event.target.price.value;
+    const product_quantity = event.target.piece.value;
+    const measurement = event.target.measurement.value;
+    const img = event.target.img.value;
+    const group = event.target.group.value;
+    console.log(group)
+  };
+
   return (
     <div className="container mx-auto mt-5 h-screen">
       <div className="flex justify-center items-center h-full">
         <div className="card card-normal w-80 md:w-96 bg-base-100 shadow-xl">
           <div className="card-body bg-stone-200">
-            <form className="grid gap-4">
+            <form onSubmit={addProduct} className="grid gap-4">
               <h2 className="card-title uppercase">Add product</h2>
               <div className="form-control">
                 <div className="input-group">
-                  <select className="select select-bordered">
+                  <select name="group" className="select select-bordered">
                     <option disabled defaultValue>
-                      Pick category
+                       Select Group
                     </option>
-                    <option>S.A GROUP</option>
-                    <option>PRAN GROUP</option>
+                    <option value="sa">S.A GROUP</option>
+                    <option value="pran">PRAN GROUP</option>
                   </select>
                 </div>
               </div>
@@ -35,8 +46,8 @@ const AddProduct = () => {
               />
               <input
                 type="number"
-                name="pieces"
-                placeholder="pieces"
+                name="piece"
+                placeholder="piece"
                 className="input border-none input-accent w-full max-w-xs"
                 required
               />
@@ -47,7 +58,16 @@ const AddProduct = () => {
                 className="input border-none input-accent w-full max-w-xs"
                 required
               />
-              <button className="btn btn-accent border-none font-bold">Button</button>
+              <input
+                type="text"
+                name="img"
+                placeholder="image link"
+                className="input border-none input-accent w-full max-w-xs"
+                required
+              />
+              <button className="btn btn-accent border-none font-bold">
+                Add Product
+              </button>
             </form>
           </div>
         </div>
